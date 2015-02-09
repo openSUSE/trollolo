3b009c4d738a43edaac5e30a8df56ddb2a959b4b
Cornelius Schumacher <cschum@suse.de>
2015-02-09 14:34:44 +0100

    Make parameters explicit

--- spec/support/webmocks.rb
+++ spec/support/webmocks.rb
@@ -1,31 +1,65 @@
 def webmock_mapping
   [
     {
-      url: 'https://api.trello.com/1/boards/myboardid?key=mykey&token=mytoken',
+      path: 'boards/myboardid',
+      parameters: {
+        "key" => "mykey",
+        "token" => "mytoken"
+      },
       file: 'board.json'
     },
     {
-      url: 'https://api.trello.com/1/boards/123?key=mykey&token=mytoken',
+      path: 'boards/123',
+      parameters: {
+        "key" => "mykey",
+        "token" => "mytoken"
+      },
       file: 'board.json'
     },
     {
-      url: 'https://api.trello.com/1/boards/53186e8391ef8671265eba9d/lists?filter=open&key=mykey&token=mytoken',
+      path: 'boards/53186e8391ef8671265eba9d/lists',
+      parameters: {
+        "filter" => "open",
+        "key" => "mykey",
+        "token" => "mytoken"
+      },
       file: 'lists.json'
     },
     {
-      url: 'https://api.trello.com/1/lists/53186e8391ef8671265eba9f/cards?filter=open&key=mykey&token=mytoken',
+      path: 'lists/53186e8391ef8671265eba9f/cards',
+      parameters: {
+        "filter" => "open",
+        "key" => "mykey",
+        "token" => "mytoken"
+      },
       file: '53186e8391ef8671265eba9f_list.json'
     },
     {
-      url: 'https://api.trello.com/1/lists/5319bf088cdf9cd82be336b0/cards?filter=open&key=mykey&token=mytoken',
+      path: 'lists/5319bf088cdf9cd82be336b0/cards',
+      parameters: {
+        "filter" => "open",
+        "key" => "mykey",
+        "token" => "mytoken"
+      },
       file: '5319bf088cdf9cd82be336b0_list.json'
     },
     {
-      url: 'https://api.trello.com/1/lists/53186e8391ef8671265eba9e/cards?filter=open&key=mykey&token=mytoken',
+      path: 'lists/53186e8391ef8671265eba9e/cards',
+      parameters: {
+        "filter" => "open",
+        "key" => "mykey",
+        "token" => "mytoken"
+      },
       file: '53186e8391ef8671265eba9e_list.json'
     },
     {
-      url: 'https://api.trello.com/1/boards/53186e8391ef8671265eba9d?cards=all&key=mykey&lists=all&token=mytoken',
+      path: 'boards/53186e8391ef8671265eba9d',
+      parameters: {
+        "cards" => "all",
+        "key" => "mykey",
+        "lists" => "all",
+        "token" => "mytoken"
+      },
       file: 'board.json'
     }
   ]
@@ -33,7 +67,15 @@ end
 
 def full_board_mock
   webmock_mapping.each do |mapping|
-    stub_request(:get, mapping[:url])
+    url = "https://api.trello.com/1/" + mapping[:path]
+    if mapping[:parameters]
+      parameters = []
+      mapping[:parameters].each do |key, value|
+        parameters.push("#{key}=#{value}")
+      end
+      url += "?" + parameters.join("&")
+    end
+    stub_request(:get, url)
       .to_return(:status => 200, :body => load_test_file(mapping[:file]))
   end
 end
