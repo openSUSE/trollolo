f674629f65dfcd8f2af12c7240f96f1679c61cf5
Cornelius Schumacher <cschum@suse.de>
2015-09-03 13:19:15 +0200

    More compact formatting

--- spec/unit/burndown_chart_spec.rb
+++ spec/unit/burndown_chart_spec.rb
@@ -3,7 +3,6 @@ require_relative 'spec_helper'
 include GivenFilesystemSpecHelpers
 
 describe BurndownChart do
-
   before(:each) do
     @settings = dummy_settings
     @burndown_data = BurndownData.new(@settings)
@@ -334,7 +333,6 @@ EOT
   end
 
   describe "reads meta data from the board" do
-
     use_given_filesystem
 
     it "merges meta data from board if present" do
@@ -354,26 +352,21 @@ EOT
   end
 
   describe '.plot' do
-
     it 'sends joined parsed options to python script' do
       allow(described_class).to receive(:process_options).and_return(%w{ --test 1 --no-blah })
       allow(described_class).to receive(:plot_helper).and_return('mescript')
       expect(described_class).to receive(:system).with('python mescript 42 --test 1 --no-blah')
       described_class.plot(42, {foo: 1, bar: 2})
     end
-
   end
 
   describe '.plot_helper' do
-
     it 'expands path to burndown generator' do
       expect(described_class.plot_helper).to include('scripts/create_burndown.py')
     end
-
   end
 
   describe '.process_options' do
-
     it 'builds an array of switches for burndown chart based on input hash' do
       test_hash = { 'no-tasks' => true }
       expect(described_class.send(:process_options, test_hash)).to eq %w{ --no-tasks }
@@ -391,6 +384,5 @@ EOT
       }
       expect(described_class.send(:process_options, test_hash)).to eq ['--no-tasks', '--with-fast-lane', '--output fanagoro', '--verbose']
     end
-
   end
 end
