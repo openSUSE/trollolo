de7b7c7eaefbc04184de8acac0c3d1072068db14
Cornelius Schumacher <cschum@suse.de>
2016-03-02 15:18:43 +0100

    Run image comparison tests in defined environment
    The integration tests comparing the images generated by the python
    script to reference images are run in a Docker environmant now, so
    that the results are better reproducible.
    
    The test were fragile because of subtle differences e.g. in how the
    anti-aliasing is done which in fact didn't change the results.

---
+++ spec/containers/matplotlib/Dockerfile
@@ -0,0 +1,6 @@
+FROM opensuse:42.1
+MAINTAINER Cornelius Schumacher <cschum@suse.de>
+
+COPY font/Humor-Sans-1.0.ttf /usr/share/fonts/truetype/
+
+RUN zypper --non-interactive install python-matplotlib python-matplotlib-tk python-PyYAML
--- spec/integration/create_burndown_spec.rb
+++ spec/integration/create_burndown_spec.rb
@@ -3,17 +3,33 @@ require_relative "integration_spec_helper"
 include GivenFilesystemSpecHelpers
 include CliTester
 
-HELPER_SCRIPT = File.expand_path("../../../scripts/create_burndown.py", __FILE__)
+def run_helper(working_dir, sprint_number, extra_args = [])
+  helper_dir = File.expand_path("../../../scripts", __FILE__)
+  args = ["run"]
+  args += ["-v", "#{helper_dir}:/trollolo/helper"]
+  args += ["-v", "#{working_dir}:/trollolo/data"]
+  args += ["-w", "/trollolo/data"]
+  args += ["matplotlib"]
+  args += ["/trollolo/helper/create_burndown.py", sprint_number]
+  args += extra_args
+  run_command(cmd: "docker", args: args)
+end
 
 describe "create_burndown.py" do
   use_given_filesystem(keep_files: true)
+
+  before(:all) do
+    if `docker images -q trollolo-matplotlib`.empty?
+      raise "Required docker image 'trollolo-matplotlib' not found. Build it with 'docker build -t trollolo-matplotlib spec/containers/matplotlib'"
+    end
+  end
 
   it "creates burndown chart for sprint 23" do
     @working_dir = given_directory do
       given_file("burndown-data-23.yaml", from: "create_burndown_helper/burndown-data-23.yaml")
     end
 
-    result = run_command(cmd: HELPER_SCRIPT, args: ["23", "--output=#{@working_dir}", "--no-head"])
+    result = run_helper(@working_dir, "23")
     expect(result).to exit_with_success("")
     expect(File.join(@working_dir, "burndown-23.png")).
       to be_same_image_as("create_burndown_helper/burndown-23.png")
@@ -24,7 +40,7 @@ describe "create_burndown.py" do
       given_file("burndown-data-31.yaml", from: "create_burndown_helper/burndown-data-31.yaml")
     end
 
-    result = run_command(cmd: HELPER_SCRIPT, args: ["31", "--output=#{@working_dir}", "--no-head"])
+    result = run_helper(@working_dir, "31")
     expect(result).to exit_with_success("")
     expect(File.join(@working_dir, "burndown-31.png")).
       to be_same_image_as("create_burndown_helper/burndown-31.png")
@@ -35,7 +51,7 @@ describe "create_burndown.py" do
       given_file("burndown-data-35.yaml", from: "create_burndown_helper/burndown-data-35.yaml")
     end
 
-    result = run_command(cmd: HELPER_SCRIPT, args: ["35", "--output=#{@working_dir}", "--no-head"])
+    result = run_helper(@working_dir, "35")
     expect(result).to exit_with_success("")
     expect(File.join(@working_dir, "burndown-35.png")).
       to be_same_image_as("create_burndown_helper/burndown-35.png")
@@ -46,8 +62,7 @@ describe "create_burndown.py" do
       given_file("burndown-data-08.yaml", from: "create_burndown_helper/burndown-data-08.yaml")
     end
 
-    result = run_command(cmd: HELPER_SCRIPT,
-      args: ["08", "--output=#{@working_dir}", "--no-tasks", "--with-fast-lane", "--no-head"])
+    result = run_helper(@working_dir, "08", ["--no-tasks", "--with-fast-lane"])
     expect(result).to exit_with_success("")
     expect(File.join(@working_dir, "burndown-08.png")).
       to be_same_image_as("create_burndown_helper/burndown-08.png")
