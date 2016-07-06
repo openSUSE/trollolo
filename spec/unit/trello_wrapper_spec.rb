61c24322f769a7b92934b06bd13306bc392adfdb
Mario Manno <mario.manno@suse.com>
2016-07-06 15:53:08 +0200

    webmock can't match body of multi-part/form-data and complains since 2.1
    See upstream issue at https://github.com/bblimke/webmock/issues/623

--- spec/unit/trello_wrapper_spec.rb
+++ spec/unit/trello_wrapper_spec.rb
@@ -65,8 +65,7 @@ EOT
           to_return(:status => 200, :body => card_body, :headers => {})
 
       stub_request(:post, "https://api.trello.com/1/cards/123/attachments?key=mykey&token=mytoken").
-          with(:body => "--470924\r\nContent-Disposition: form-data; name=\"file\"; filename=\"attachment-data\"\r\nContent-Type: text/plain\r\n\r\nabc\n\r\n--470924\r\nContent-Disposition: form-data; name=\"name\"\r\n\r\n\r\n--470924--\r\n",
-               :headers => {'Accept'=>'*/*; q=0.5, application/xml', 'Accept-Encoding'=>'gzip, deflate', 'Content-Length'=>'188', 'Content-Type'=>'multipart/form-data; boundary=470924', 'User-Agent'=>'Ruby'}).
+          with(:headers => {'Accept'=>'*/*; q=0.5, application/xml', 'Accept-Encoding'=>'gzip, deflate', 'Content-Length'=>'188', 'Content-Type'=>'multipart/form-data; boundary=470924', 'User-Agent'=>'Ruby'}).
             to_return(:status => 200, :body => "", :headers => {})
 
       path = given_file("attachment-data")
