3d5909c2147f72b833b9179971c0c090bb1d0ab1
Mario Manno <mario.manno@suse.com>
2016-07-19 12:37:42 +0200

    improve vcr rerecord handling

--- spec/unit/support/vcr.rb
+++ spec/unit/support/vcr.rb
@@ -4,12 +4,47 @@ VCR.configure do |config|
   config.hook_into :webmock
 end
 
+# example needs to use real_settings if vcr_rerecord: true is used
+def real_settings
+  config_path = ENV["TROLLOLO_CONFIG_PATH"] || File.expand_path("~/.trollolorc")
+  Settings.new(config_path)
+end
+
+def real_settings_needed?(example, subject)
+  example.metadata[:vcr_rerecord] && subject.instance_variable_get(:@settings).developer_public_key == 'mykey'
+end
+
+def cassette_path(cassette)
+  File.join(VCR.configuration.cassette_library_dir, cassette + '.yml')
+end
+
+def vcr_rerecord?(example)
+  example.metadata[:vcr_rerecord] && File.readable?(cassette_path(example.metadata[:vcr]))
+end
+
+def vcr_record_mode(example)
+  return :all if vcr_rerecord?(example)
+  :none
+end
+
+def vcr_replace_tokens(cassette_path)
+  settings = real_settings
+  text = File.read(cassette_path)
+  File.open(cassette_path, 'w') do |f|
+    text.gsub!(settings.member_token, 'mytoken')
+    text.gsub!(settings.developer_public_key, 'mykey')
+    f.print text
+  end
+end
+
 RSpec.configure do |c|
   c.around do |example|
     if cassette = example.metadata[:vcr]
-      VCR.use_cassette(cassette) do
+      fail "you need to use real_settings to re-record vcr data" if real_settings_needed?(example, subject)
+      VCR.use_cassette(cassette, record: vcr_record_mode(example)) do
         example.run
       end
+      vcr_replace_tokens(cassette_path(cassette)) if vcr_rerecord?(example)
     else
       example.run
     end
