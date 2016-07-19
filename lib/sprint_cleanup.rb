d33fb69d3f057758be8c782b304588e8152e541d
Mario Manno <mario.manno@suse.com>
2016-07-19 12:46:25 +0200

    remove waterline label and members on sprint cleanup

--- lib/sprint_cleanup.rb
+++ lib/sprint_cleanup.rb
@@ -33,10 +33,21 @@ class SprintCleanup
     card.labels.any? { |l| l.name == "Sticky" }
   end
 
+  def waterline_label(card)
+    card.labels.find { |label| label.name =~ /waterline/i }
+  end
+
+  def remove_waterline_label(card)
+    label = waterline_label(card)
+    card.remove_label(label) if label
+  end
+
   def move_cards(source_list)
     source_list.cards.each do |card|
       next if sticky?(card)
       puts %(moving card "#{card.name}" to list "#{target_list.name}")
+      card.members.each { |member| card.remove_member(member) }
+      remove_waterline_label(card)
       card.move_to_board(@target_board, target_list)
     end
   end
--- spec/unit/sprint_cleanup_spec.rb
+++ spec/unit/sprint_cleanup_spec.rb
@@ -2,11 +2,6 @@ require_relative "spec_helper"
 require 'pry'
 
 describe SprintCleanup do
-  let(:real_settings) {
-    config_path = ENV["TROLLOLO_CONFIG_PATH"] || File.expand_path("~/.trollolorc")
-    Settings.new(config_path)
-  }
-
   it "creates new sprint cleanup" do
     sprint_cleanup = SprintCleanup.new(dummy_settings)
     expect(sprint_cleanup).to be
@@ -15,8 +10,8 @@ describe SprintCleanup do
   context "default" do
     subject { described_class.new(dummy_settings) }
 
-    it "moves remaining cards to target board", vcr: "sprint_cleanup" do
-      expect(subject.cleanup("gyp75UHZ", "neUHHzDo")).to be
+    it "moves remaining cards to target board", vcr: "sprint_cleanup", vcr_rerecord: false do
+      expect(subject.cleanup("GVMQz9dx", "neUHHzDo")).to be
     end
   end
 end
