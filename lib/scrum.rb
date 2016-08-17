66aeb3833167d736da627f303194072951713b00
Mario Manno <mario.manno@suse.com>
2016-08-17 16:34:20 +0200

    use require relative, fixes bug with bin/trollolo

--- lib/scrum.rb
+++ lib/scrum.rb
@@ -1,10 +1,10 @@
 module Scrum
 end
 
-require 'scrum/card_type_detection'
-require 'scrum/sprint_board'
-require 'scrum/sprint_planning_board'
+require_relative 'scrum/card_type_detection'
+require_relative 'scrum/sprint_board'
+require_relative 'scrum/sprint_planning_board'
 
-require 'scrum/prioritizer'
-require 'scrum/sprint_cleanup'
-require 'scrum/move_backlog'
+require_relative 'scrum/prioritizer'
+require_relative 'scrum/sprint_cleanup'
+require_relative 'scrum/move_backlog'
