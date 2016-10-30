b13c119f21e1f064d27a7665cfd09501f9a61134
Björn Geuken <bgeuken@suse.de>
2016-10-30 22:48:08 +0100

    Move checklist related code from Card to Checklist class
    Simplify tasks and done_tasks methods of Card class by:
    
      * Moving checklist related code to Checklist class
      * Moving checklist filtering into helper method (filtered_checklists)

--- lib/card.rb
+++ lib/card.rb
@@ -44,27 +44,11 @@ class Card
   end
 
   def done_tasks
-    count = 0
-    @card_data["checklists"].each do |checklist|
-      unless (@settings && @settings.no_task_checklists.include?(checklist["name"]))
-        checklist["checkItems"].each do |checklist_item|
-          if checklist_item["state"] == "complete"
-            count += 1
-          end
-        end
-      end
-    end
-    count
+    filtered_checklists.map(&:done_tasks).sum
   end
 
   def tasks
-    count = 0
-    @card_data["checklists"].each do |checklist|
-      unless (@settings && @settings.no_task_checklists.include?(checklist["name"]))
-        count += checklist["checkItems"].count
-      end
-    end
-    count
+    filtered_checklists.map(&:tasks).sum
   end
 
   def card_labels
@@ -74,6 +58,12 @@ class Card
   def checklists
     @card_data["checklists"].map do |checklist|
       Checklist.new(checklist)
+    end
+  end
+
+  def filtered_checklists
+    checklists.reject do |checklist|
+      @settings && @settings.no_task_checklists.include?(checklist.name)
     end
   end
 
