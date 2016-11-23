5f26869972bc158634b8c520b0103b8632f7f5a2
Björn Geuken <bgeuken@suse.de>
2016-11-23 22:04:36 +0100

    Remove superfluous whitelines

--- spec/unit/scrum/prioritizer_spec.rb
+++ spec/unit/scrum/prioritizer_spec.rb
@@ -31,7 +31,7 @@ describe Scrum::Prioritizer do
         "5319c16d9d04708d450d65f1/name?key=mykey&token=mytoken&value=(3)%20P2:%20Fill%20Backlog%20column",
         "5319c57ff6be845f428aa7a3/name?key=mykey&token=mytoken&value=(5)%20P3:%20Read%20data%20from%20Trollolo",
         "5319c5961e530fd26f83999d/name?key=mykey&token=mytoken&value=(3)%20P4:%20Save%20read%20data%20as%20reference%20data",
-				"5319c5a8743488047e13fcbc/name?key=mykey&token=mytoken&value=(8)%20P5:%20Celebrate%20testing%20board",
+        "5319c5a8743488047e13fcbc/name?key=mykey&token=mytoken&value=(8)%20P5:%20Celebrate%20testing%20board",
       ].each { |value|
         stub_request(:put, "https://api.trello.com/1/cards/#{value}")
       }
