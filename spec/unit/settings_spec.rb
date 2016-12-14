b02db69dbc25b9794daddf80a25a648052834c30
Mario Manno <mario.manno@suse.com>
2016-12-14 15:53:38 +0100

    Add scrum setup names to settings

--- spec/unit/settings_spec.rb
+++ spec/unit/settings_spec.rb
@@ -3,12 +3,12 @@ require_relative 'spec_helper'
 describe Settings do
 
   include GivenFilesystemSpecHelpers
-  
+
   context "given config file" do
     before(:each) do
       @settings = Settings.new( File.expand_path('../../data/trollolorc',__FILE__) )
     end
-    
+
     it "is not verbose by default" do
       expect(@settings.verbose).to be false
     end
@@ -16,6 +16,24 @@ describe Settings do
     it "reads config file" do
       expect(@settings.developer_public_key).to eq "mykey"
       expect(@settings.member_token).to eq "mytoken"
+    end
+
+    context "#scrum" do
+      context "when setting is missing" do
+        before do
+          @settings = Settings.new( File.expand_path('../../data/trollolorc_with_board_aliases',__FILE__) )
+        end
+        it "returns default settings" do
+          expect(@settings.scrum["board_names"]).to eq({"planning"=>"Planning Board", "sprint"=>"Sprint Board"})
+        end
+      end
+
+      context "when setting does exist" do
+        it "returns name" do
+          expect(@settings.scrum["board_names"]["sprint"]).to eq("Sprint Board")
+          expect(@settings.scrum.board_names["planning"]).to eq("Planning Board")
+        end
+      end
     end
 
     context "#board_aliases" do
@@ -36,10 +54,10 @@ describe Settings do
       end
     end
   end
-  
+
   context "non-existent config file" do
     use_given_filesystem
-  
+
     before(:each) do
       @config_file = given_dummy_file
       @settings = Settings.new(@config_file)
@@ -49,7 +67,7 @@ describe Settings do
       @settings.developer_public_key = "mypublickey"
       @settings.member_token = "mymembertoken"
       @settings.save_config
-      
+
       expect(File.read(@config_file)).to eq "---\ndeveloper_public_key: mypublickey\nmember_token: mymembertoken\n"
     end
   end
