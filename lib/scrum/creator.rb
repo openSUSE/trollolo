bbc14efa0c5046c710f09aea5b3697880522f7e6
Mario Manno <mario.manno@suse.com>
2016-12-15 13:50:38 +0100

    Add test data  for creator spec

--- lib/scrum/creator.rb
+++ lib/scrum/creator.rb
@@ -2,15 +2,14 @@ module Scrum
   class Creator < TrelloService
     def create
       @scrum = @settings.scrum
-      create_labels
       create_sprint_board
       create_planning_board
     end
 
     private
 
-    def create_labels
-      @scrum.label_names.each { |_, name| Trello::Label.create(name: name) }
+    def create_labels(board_id)
+      @scrum.label_names.each { |_, name| Trello::Label.create(name: name, board_id: board_id) }
     end
 
     def create_sprint_board
@@ -18,13 +17,14 @@ module Scrum
       Trello::List.create(board_id: board.id, name: @scrum.list_names["sprint_backlog"])
       Trello::List.create(board_id: board.id, name: @scrum.list_names["sprint_qa"])
       Trello::List.create(board_id: board.id, name: @scrum.list_names["sprint_doing"])
+      create_labels(board.id)
     end
 
     def create_planning_board
       board = Trello::Board.create(name: @scrum.board_names["planning"])
       Trello::List.create(board_id: board.id, name: @scrum.list_names["planning_backlog"])
       Trello::List.create(board_id: board.id, name: @scrum.list_names["planning_ready"])
-      Scrum::SprintPlanningBoard.new(@scrum.scrum).create!(planning_board)
+      create_labels(board.id)
     end
   end
 end
--- spec/unit/scrum/creator_spec.rb
+++ spec/unit/scrum/creator_spec.rb
@@ -2,6 +2,11 @@ require_relative "../spec_helper"
 
 describe Scrum::Creator do
   subject { described_class.new(dummy_settings) }
+  let(:custom_subject) {
+    custom_settings = dummy_settings
+    custom_settings.scrum.board_names["planning"] = "Planungs Brett"
+    described_class.new(custom_settings)
+  }
 
   it "creates new creator" do
     expect(subject).to be
@@ -12,7 +17,7 @@ describe Scrum::Creator do
       expect { subject.create }.not_to raise_error
     end
     it "creates boards according to existing config", vcr: "creator_custom_config", vcr_record: false do
-      expect { subject.create }.not_to raise_error
+      expect { custom_subject.create }.not_to raise_error
     end
   end
 end
